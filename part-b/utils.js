570f20af563b175055819be866e63a34164a4426
Ondrej Vostal <ondrej@vostal.net>
2021-09-18 16:43:48 +0200

    [part-b] Fix utils.js for london fork

--- part-b/utils.js
+++ part-b/utils.js
@@ -1,4 +1,5 @@
 const { alchemy_endpoint } = require('./config.js');
+const parseEther = ethers.utils.parseEther;
 
 
 async function pinBlock(number) {
@@ -25,6 +26,14 @@ async function impersonate(who) {
 }
 
 
+async function setBalance(address, amount) {
+  return await network.provider.send(
+    'hardhat_setBalance',
+    [address, ethers.utils.hexlify(amount)]
+  );
+}
+
+
 async function getERC20(address) {
   return await ethers.getContractAt(
     '@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20',
@@ -37,11 +46,15 @@ async function getERC20(address) {
 const tokenHolders = {
   /* WETH is in WETH */
   '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2': '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+  /* DAI is in DAI */
+  '0x6b175474e89094c44da98b954eedeac495271d0f': '0x6B175474E89094C44Da98b954EedeAC495271d0F',
 };
 const BINANCE_ADDRESS = '0x3f5CE5FBFe3E9af3971dD833D26bA9b5C936f0bE';
+const KRAKEN_ADDRESS = '0x2910543Af39abA0Cd09dBb2D50200b3E800A63D2';
 
 
 async function sendTokens(sendToAddr, amount, tokenAddr) {
+
   const tokenAddrLower = tokenAddr.toLowerCase();
   const holder = tokenAddrLower in tokenHolders
         ? tokenHolders[tokenAddrLower]
@@ -53,7 +66,8 @@ async function sendTokens(sendToAddr, amount, tokenAddr) {
   if (holderBalance < amount)
     throw `${holder} has too few ${tokenAddr}`;
 
-  await token.connect(holderSigner).transfer(sendToAddr, amount, {gasPrice: 0});
+  await setBalance(holder, parseEther('10')); // we need to have some ETH for basefee
+  await token.connect(holderSigner).transfer(sendToAddr, amount);
 
   await token.balanceOf(holder);
 }
@@ -63,4 +77,5 @@ module.exports = {
   pinBlock,
   impersonate,
   sendTokens,
+  getERC20,
 }
